893bd55ae53e68459bf4607a6a400f055daeecf0
zhaotli <zhaotli@student.ethz.ch>
2023-01-11 16:10:02 +0100

    add obstacles during planning

--- moveit_tutorials/doc/move_group_interface/src/move_group_cpp_trajectory_generation.cpp
+++ moveit_tutorials/doc/move_group_interface/src/move_group_cpp_trajectory_generation.cpp
@@ -120,6 +120,49 @@ int main(int argc, char** argv)
   // ^^^^^^^^^^^^^^^^^^^^^^^^^
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
+
+  // Now let's define a collision object ROS message for the robot to avoid.
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = move_group_interface.getPlanningFrame();
+
+  // The id of the object is used to identify it.
+  collision_object.id = "box1";
+
+  // Define a box to add to the world.
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[primitive.BOX_X] = 0.2;
+  primitive.dimensions[primitive.BOX_Y] = 0.1;
+  primitive.dimensions[primitive.BOX_Z] = 0.05;
+
+  // Define a pose for the box (specified relative to frame_id)
+  geometry_msgs::Pose box_pose;
+  box_pose.orientation.w = 1.0;
+  box_pose.position.x = 0.45;
+  box_pose.position.y = 0.0;
+  box_pose.position.z = 0.65;
+
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
+
+  // Now, let's add the collision object into the world
+  // (using a vector that could contain additional objects)
+  ROS_INFO_NAMED("tutorial", "Add an object into the world");
+  planning_scene_interface.addCollisionObjects(collision_objects);
+
+  // Show text in RViz of status and wait for MoveGroup to receive and process the collision object message
+  visual_tools.publishText(text_pose, "Add object", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to once the collision object appears in RViz");
+
+
+
+
   // .. _move_group_interface-planning-to-pose-goal:
   //
   // Planning to a Pose goal
@@ -131,7 +174,7 @@ int main(int argc, char** argv)
   target_pose1.orientation.y = 0.3826834;
   target_pose1.position.x = 0.5;
   target_pose1.position.y = 0.0;
-  target_pose1.position.z = 0.59;
+  target_pose1.position.z = 0.49;
   move_group_interface.setPoseTarget(target_pose1);
 
   // Now, we call the planner to compute the plan and visualize it.
